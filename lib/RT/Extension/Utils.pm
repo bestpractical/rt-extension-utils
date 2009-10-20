00cda1b272e2845081785a74abea25f8db39d0f6
Ruslan Zakirov <ruz@bestpractical.com>
2009-10-21 02:23:03 +0400

    skeleton

---
+++ lib/RT/Extension/Utils.pm
@@ -0,0 +1,30 @@
+use 5.008003;
+use strict;
+use warnings;
+
+package RT::Extension::Utils;
+
+=head1 NAME
+
+RT::Extension::Utils - collection of command line utilities
+
+=head1 DESCRIPTION
+
+This extension is collection of variouse command line utilities
+for Request Tracker. Mostly these are tools for system administrators
+for debugging and instrumenting some tasks.
+
+=cut
+
+
+=head1 LICENSE
+
+Under the same terms as perl itself.
+
+=head1 AUTHOR
+
+Ruslan Zakirov E<lt>Ruslan.Zakirov@gmail.comE<gt>
+
+=cut
+
+1;
